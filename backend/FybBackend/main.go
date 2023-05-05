eb456e85a2f431fc910108c2a3a7f130ac7124e7
FishinTroubledWater <101721304+FishinTroubledWater@users.noreply.github.com>
2023-05-05 19:08:36 +0800

    Update main.go 解决跨域问题

--- backend/FybBackend/main.go
+++ backend/FybBackend/main.go
@@ -9,17 +9,53 @@ import (
 	"FybBackend/routers/v1/frontend/user/userInfo"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+			c.Header("Access-Control-Max-Age", "172800")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		if method == "OPTIONS" {
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Panic info is: %v", err)
+			}
+		}()
+
+		c.Next()
+	}
+}
 
 func main() {
 	db := fybDatabase.InitDB()
 	r := gin.Default()
+	r.Use(Cors())
 	academy.SearchByRule(r)
 	login.PasswordLogin(r, db)
 	selectUsers.SelectUsers(r, db)
 	userInfo.BasicUserInfo(r, db)
 	settings.Settings(r, db)
-	if err := r.Run(); err != nil {
+
+	if err := r.Run(":8088"); err != nil {
 		fmt.Println("startup service failed, err:%v\n", err)
 	}
 
