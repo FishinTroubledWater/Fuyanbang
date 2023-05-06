6095edb2da0fae9ea2af03189dcaf4d42df3a4f5
可林Kellin <1721298496@qq.com>
2023-05-06 22:50:47 +0800

    完善Token相关函数
    完善Token相关函数

--- backend/FybBackend/database/crud.go
+++ backend/FybBackend/database/crud.go
@@ -85,6 +85,19 @@ func SelectSingleUserByCondition(db *gorm.DB, where map[string]interface{}) (Use
 	err := db.Where(where).First(&user).Count(&count).Error
 	return user, count, err
 }
+func SelectAllUserByCondition(db *gorm.DB, where map[string]interface{}) ([]User, int64, error) {
+	var count int64 = 0
+	var users []User
+	err := db.Where(where).Find(&users).Count(&count).Error
+	return users, count, err
+}
+func SelectAllUserByPage(db *gorm.DB, pageNum int64, pageSize int64) ([]User, int64, error) {
+	var count int64 = 0
+	var users []User
+	err := db.Table("user").Where(" id >= ? and id <= ?", (pageNum-1)*pageSize, pageNum*pageSize).Find(&users).Count(&count).Error
+	return users, count, err
+}
+
 func SelectSingleAdminByCondition(db *gorm.DB, where map[string]interface{}) (Admin, int64, error) {
 	var count int64 = 0
 	var admin Admin
@@ -96,12 +109,7 @@ func UpdateSingleAdminByCondition(db *gorm.DB, where map[string]interface{}, upd
 	err := db.Table("admin").Where(where).Updates(update).Count(&count).Error
 	return count, err
 }
-func SelectAllUserByCondition(db *gorm.DB, where map[string]interface{}) ([]User, int64, error) {
-	var count int64 = 0
-	var users []User
-	err := db.Where(where).Find(&users).Count(&count).Error
-	return users, count, err
-}
+
 func UpdateSingleUserByCondition(db *gorm.DB, where map[string]interface{}, update map[string]interface{}) (int64, error) {
 	var count int64 = 0
 	err := db.Table("user").Where(where).Updates(update).Count(&count).Error
--- backend/FybBackend/routers/backend.go
+++ backend/FybBackend/routers/backend.go
@@ -2,10 +2,12 @@ package routers
 
 import (
 	"FybBackend/routers/v1/backend/user/login"
+	"FybBackend/routers/v1/backend/user/selectUsersByPage"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func InitBackend(r *gin.Engine, db *gorm.DB) {
 	login.Login(r, db)
+	selectUsersByPage.SelectUsersByPage(r, db)
 }
