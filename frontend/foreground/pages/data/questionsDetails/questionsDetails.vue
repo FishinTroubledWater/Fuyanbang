ced92cef367045723651718d0e509655e1abafd3
zhangjiajun <93666885+fzubooker@users.noreply.github.com>
2023-05-07 22:36:30 +0800

    update

---
+++ frontend/foreground/pages/data/questionsDetails/questionsDetails.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view>这里是问题详情界面</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
