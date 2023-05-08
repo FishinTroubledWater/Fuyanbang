2c74f1ddd61d7bfd916f3e42d6731f97f474cded
可林Kellin <1721298496@qq.com>
2023-05-08 15:35:25 +0800

    修复了后台用户查询的BUG
    修复了后台用户查询的BUG

--- backend/FybBackend/routers/v1/backend/user/selectUsersByPage/selectUsersByPage.go
+++ backend/FybBackend/routers/v1/backend/user/selectUsersByPage/selectUsersByPage.go
@@ -2,6 +2,7 @@ package selectUsersByPage
 
 import (
 	fybDatabase "FybBackend/database"
+	"FybBackend/routers/v1/backend/user/token"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/go-multierror"
@@ -17,23 +18,23 @@ type responseItem struct {
 func SelectUsersByPage(e *gin.Engine, db *gorm.DB) {
 	e.POST("/v1/backend/user/list", func(context *gin.Context) {
 		var errors *multierror.Error
-		mp := make(map[string]int64)
+		mp := make(map[string]interface{})
 		b, err1 := context.GetRawData()
 		err2 := json.Unmarshal(b, &mp)
-		//err3 := token.JwtVerify(context)
-		//pageNum := mp["pageNum"]
-		//pageSize := mp["pageSize"]
-		//users, count, err4 := fybDatabase.SelectAllUserByPage(db, pageNum, pageSize)
-		errors = multierror.Append(errors, err1, err2)
+		err3 := token.JwtVerify(context)
+		pageNum := int64(mp["pageNum"].(float64))
+		pageSize := int64(mp["pageSize"].(float64))
+		users, count, err4 := fybDatabase.SelectAllUserByPage(db, pageNum, pageSize)
+		errors = multierror.Append(errors, err1, err2, err3, err4)
 		if errors.ErrorOrNil() == nil {
 			context.JSON(200, gin.H{
 				"code":    200,
 				"message": "get userInfoList success!",
-				//"data": responseItem{
-				//	Total:   count,
-				//	PageNum: pageNum,
-				//	Users:   users,
-				//},
+				"data": responseItem{
+					Total:   count,
+					PageNum: pageNum,
+					Users:   users,
+				},
 			})
 		} else {
 			context.JSON(404, gin.H{
