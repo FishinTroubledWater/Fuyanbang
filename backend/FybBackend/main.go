49c52a6dd1e9e4c05cdea1274d9b918cb222def5
可林Kellin <1721298496@qq.com>
2023-05-10 09:30:55 +0800

    解决部分请求方法的跨域问题
    解决部分请求方法的跨域问题

--- backend/FybBackend/main.go
+++ backend/FybBackend/main.go
@@ -24,7 +24,7 @@ func Cors() gin.HandlerFunc {
 		if method == "OPTIONS" {
 			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization")
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
 			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 			c.Header("Access-Control-Allow-Credentials", "true")
 			c.AbortWithStatus(http.StatusNoContent)
