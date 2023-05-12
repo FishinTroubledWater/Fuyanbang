0996c0447f03874c33ed6da5ebc6e8a2e38d6dff
JcTangka <1820342025@qq.com>
2023-05-12 21:06:50 +0800

    About样式调整
    About样式调整

--- frontend/foreground/pages/my/aboutUs.vue
+++ frontend/foreground/pages/my/aboutUs.vue
@@ -42,7 +42,7 @@
 		</u-popup>
 		<view class="about_60">
 			<view class="about_62">
-				<image src="/static/my-assets//about/Logo.jpg" mode="scaleToFill" border="0" class="about_22"></image>
+				<image src="/static/my-assets//about//Logo.jpg" mode="scaleToFill" border="0" class="about_22"></image>
 				<text decode="true" class="about_35">福研帮</text>
 				<text decode="true" class="about_36">{{version}}</text>
 			</view>
@@ -146,8 +146,8 @@
 		width: 609upx;
 		height: 300upx;
 		padding: 0upx;
-		margin-top: 127upx;
-		margin-left: 84upx;
+		margin-top: 100upx;
+		margin-left: 9%;
 		float: left;
 		position: absolute;
 		top: 0upx;
