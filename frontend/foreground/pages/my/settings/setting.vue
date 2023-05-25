d5c68fd85dcaa400c771a060993cb9dede63c443
JcTangka <1820342025@qq.com>
2023-05-25 21:52:40 +0800

    清除缓存
    清除缓存

--- frontend/foreground/pages/my/settings/setting.vue
+++ frontend/foreground/pages/my/settings/setting.vue
@@ -1,11 +1,13 @@
 <template>
 	<view class="container">
-		<view class="list-cell b-b m-t" @click="gotoPage('/pages/my/settings/userInfo')" hover-class="cell-hover" :hover-stay-time="50">
+		<view class="list-cell b-b m-t" @click="gotoPage('/pages/my/settings/userInfo')" hover-class="cell-hover"
+			:hover-stay-time="50">
 			<text class="cell-tit">个人资料</text>
 			<text class="cell-more yticon icon-you"></text>
 			<text>></text>
 		</view>
-		<view class="list-cell b-b" @click="gotoPage('/pages/my/settings/accountSecurity')" hover-class="cell-hover" :hover-stay-time="50">
+		<view class="list-cell b-b" @click="gotoPage('/pages/my/settings/accountSecurity')" hover-class="cell-hover"
+			:hover-stay-time="50">
 			<text class="cell-tit">账号与安全</text>
 			<text class="cell-more yticon icon-you"></text>
 			<text>></text>
@@ -15,12 +17,13 @@
 			<text class="cell-tit">消息推送</text>
 			<switch checked color="#fa436a" @change="switchChange" />
 		</view>
-		<view class="list-cell m-t b-b" hover-class="cell-hover" :hover-stay-time="50">
+		<view class="list-cell m-t b-b" @click="cleanCache" hover-class="cell-hover" :hover-stay-time="50">
 			<text class="cell-tit">清除缓存</text>
-			<text class="cell-tip">105.4M</text>
+			<text class="cell-tip">{{cache}}M</text>
 			<text class="cell-more yticon icon-you"></text>
 		</view>
-		<view class="list-cell b-b" @click="gotoPage('/pages/my/settings/aboutUs')"hover-class="cell-hover" :hover-stay-time="50">
+		<view class="list-cell b-b" @click="gotoPage('/pages/my/settings/aboutUs')" hover-class="cell-hover"
+			:hover-stay-time="50">
 			<text class="cell-tit">关于我们</text>
 			<text class="cell-more yticon icon-you"></text>
 			<text>></text>
@@ -35,20 +38,39 @@
 	import {
 		mapMutations
 	} from 'vuex';
+import { getTransitionRawChildren } from "vue";
 	export default {
 		data() {
 			return {
+				max:100,
+				min:12,
+				rand:0,
+				cache:105.2
 
 			};
 		},
+		onLoad() {
+			this.getRand();
+			this.cache +=this.rand;
+		},
 		methods: {
-			logout(){
+			cleanCache() {
+				this.cache = 12.8;
+				uni.showToast({
+					title: '清除缓存成功',
+					//将值设置为 success 或者直接不用写icon这个参数
+					icon: 'success',
+					//显示持续时间为 1.5秒
+					duration: 1500
+				})
+			},
+			logout() {
 				uni.clearStorage();
-				setTimeout(()=>{
+				setTimeout(() => {
 					uni.navigateTo({
-						url:'/pages/login/passwordLogin'
+						url: '/pages/login/passwordLogin'
 					})
-				},200)
+				}, 200)
 			},
 			gotoPage(url) {
 				uni.navigateTo({
@@ -71,15 +93,22 @@
 				let statusTip = e.detail.value ? '打开' : '关闭';
 				this.$api.msg(`${statusTip}消息推送`);
 			},
+			getRand() {
+				console.log(this.min)
+				console.log(this.max)
+				this.rand = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min
+
+			}
 
 		}
 	}
 </script>
 
 <style lang='scss'>
-	page{
+	page {
 		background: $page-color-base;
 	}
+
 	.list-cell {
 		display: flex;
 		align-items: baseline;
